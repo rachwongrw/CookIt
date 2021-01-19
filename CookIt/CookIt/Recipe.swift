f1659b0104914278918104689176f7d38d015bab
rachwongrw <rwrachelwong@gmail.com>
2021-01-19 11:29:45 -0800

    add a recipes controller

---
+++ CookIt/CookIt/Recipe.swift
@@ -0,0 +1,9 @@
+class Recipe {
+  var title: String
+  var steps: [String]
+  
+  init(title: String, steps: [String]) {
+    self.title = title
+    self.steps = steps
+  }
+}
--- CookIt/CookIt/ViewController.swift
+++ CookIt/CookIt/ViewController.swift
@@ -11,23 +11,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
   // any items after the first class (inheritance) are protocols.
 
   @IBOutlet weak var tableView: UITableView!
+  let recipesOld: [String] = ["Best Brownies", "Banana Bread", "Chocolate Chip Cookies"]
+  
+  var brownie = Recipe(title: "Best Brownies", steps: [])
+  var bBread = Recipe(title: "Banana Bread", steps: [])
+  var cookies = Recipe(title: "Chocolate Chip Cookies", steps: [])
+  
+  var recipes: [Recipe] = [Recipe(title: "Best Brownies", steps: []), Recipe(title: "Banana Bread", steps: []), Recipe(title: "Chocolate Chip Cookies", steps: [])]
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     tableView.delegate = self
     tableView.dataSource = self
+  
   }
-
+  
+//  var recipes = [brownie, bBread, cookies]
   
   // ----- protocol stubs here -----
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 8 // returns the no. of rows in the section
+    return recipes.count // returns the no. of rows in the section
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "recipeCell", for: indexPath) // this looks for the reusable cell
-    cell.textLabel?.text = "Hello"
+    
+//    for recipe in recipes {
+    cell.textLabel?.text = recipes[indexPath.row].title
+//    }
+    
     return cell
   }
  // --------------------------------
