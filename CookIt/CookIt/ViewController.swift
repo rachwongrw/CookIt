6b1c53b4efa1f1f5f49ffdba5263140be51d447b
rachwongrw <rwrachelwong@gmail.com>
2021-01-19 09:51:55 -0800

    add proj files

---
+++ CookIt/CookIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookIt
+//
+//  Created by Rachel Wong on 19/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
